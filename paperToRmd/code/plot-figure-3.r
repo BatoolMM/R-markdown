a4ab2344f846a52094569ad9cef4a42097b8a0d3
torwhite <whitet@ucsb.edu>
2020-12-11 16:20:12 -0800

    edits to the paper template and r scripts for code chunks

--- paperToRmd/code/plot-figure-3.r
+++ paperToRmd/code/plot-figure-3.r
@@ -1,10 +1,11 @@
 library(tidyverse)
 
-data <- read_csv("../data/figure-3-data.csv", col_types="fi") %>%
-  mutate(procedure=fct_reorder(procedure, responses))
+data3 <- read_csv("../data/figure-3-data.csv", col_types="fi") 
 
-ggplot(data, aes(x=procedure, y=responses)) +
-  geom_bar(stat="identity", fill="blue") +
+data3 %>%
+  mutate(procedure=fct_reorder(procedure, responses)) %>%
+  ggplot(data, mapping = aes(x=procedure, y=responses)) +
+  geom_bar(stat="identity", fill="#377eb8") +
   coord_flip() +
   scale_x_discrete(labels = function(x) str_wrap(x, width=30)) +
   xlab("Procedures/Activities Cores Would Like To Implement") +
